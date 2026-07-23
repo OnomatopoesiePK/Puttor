61e56b297f0f337ca09402b4812030af0c61a9f7
Paul Kaineder <paul@kaineder.cc>
2026-07-23 09:41:02 +0200

    Initial Commit

---
+++ Puttor/PuttorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuttorApp.swift
+//  Puttor
+//
+//  Created by Paul Kaineder on 23.07.26.
+//
+
+import SwiftUI
+
+@main
+struct PuttorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PuttorTests/PuttorTests.swift
@@ -0,0 +1,19 @@
+//
+//  PuttorTests.swift
+//  PuttorTests
+//
+//  Created by Paul Kaineder on 23.07.26.
+//
+
+import Testing
+@testable import Puttor
+
+struct PuttorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
